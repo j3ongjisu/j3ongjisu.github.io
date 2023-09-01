cad3b6afa32573ddaa7e1c13c39f03d26e5c40cd
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-09-01 15:52:45 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function () {
+
+    $('.main_content').fullpage({
+        anchors: ['main', 'sub01', 'sub02', 'sub03', 'sub04', 'sub05', 'sub06', 'sub07'],
+        navigation: false,
+        css3: false,
+        //반응형에서 fullpage 안하기.
+        responsiveWidth: 700,
+        //넘치는 부분 스크롤 하기.
+        scrollOverflow: true,
+        // easing: 'easeOutBounce',
+        //easingCss3: 'cubic-bezier(0.785, 0.135, 0.15, 0.86)',
+        afterRender: function () {
+            $('.main_content .section').eq(0).addClass('on');
+        },
+        afterLoad: function (lnk, idx) {
+            console.log(lnk, idx);
+            // $('.gnb li').eq(idx - 1).addClass('on').siblings().removeClass('on');
+            $('.main_content .section').eq(idx - 1).addClass('on').siblings().removeClass('on');
+        },
+        onLeave: function (idx, nidx, dir) {
+            $('.gnb li').eq(nidx - 1).addClass('on').siblings().removeClass('on');
+            console.log(idx, nidx, dir);
+
+            if (dir == 'up') {
+                $('.header').addClass('on')
+            } else {
+                $('.header').removeClass('on')
+            }
+
+        }
+    });
+
+})
